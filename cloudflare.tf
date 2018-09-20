7a311234de1b862265794da2b5bb8b7539aebb86
JarmoPerttunen <jarmo.perttunen@kolumbus.fi>
2018-09-20 14:33:36 +0300

    terraform demo, added jiipeedomain.online

--- cloudflare.tf
+++ cloudflare.tf
@@ -2,3 +2,12 @@ provider "cloudflare" {
   email = "${var.cloudflare_email}"
   token = "${var.cloudflare_token}"
 }
+
+resource "cloudflare_record" "www" {
+  domain  = "${var.cloudflare_zone}"
+  name    = "www"
+  value   = "${google_compute_address.default.address}"
+  type    = "A"
+  ttl     = 1
+  proxied = true
+}
